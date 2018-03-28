f61c137ca4f5177d654868e952ed90b839a525dd
Andrea Santurbano <santand@gmail.com>
2018-03-28 09:52:29 +0200

    Added Kafka Embedded for the tests (#1)
    Externalized the configuration
    Updated the readme.adoc

--- src/main/kotlin/kafka/KafkaConfiguration.kt
+++ src/main/kotlin/kafka/KafkaConfiguration.kt
@@ -2,14 +2,44 @@ package kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.neo4j.kernel.configuration.Config
 import java.util.*
 
-data class KafkaConfiguration(val zookeeperHosts: String = "localhost:2181", val kafkaHosts: String = "localhost:9092",
-                                     val acks: String = "1", val partitionSize: Int = 1,
-                                     val retries: Int = 2, val kafkaBatchSize: Int = 16384,
-                                     val kafkaBufferSize: Int = 33554432, val reindexBatchSize: Int = 1000,
-                                     val sessionTimeoutMs: Int = 15 * 1000, val connectTimeoutMs: Int = 10 * 1000,
-                                     val topic: String = "neo4j", val replication: Int = 1, val groupId : String = "neo4j") {
+data class KafkaConfiguration(val kafkaHosts: String = DEFAULTS.get("kafka.bootstrap.servers").toString(),
+                              val acks: String = DEFAULTS.get("kafka.acks").toString(),
+                              val retries: Int = DEFAULTS.get("kafka.retries").toString().toInt(),
+                              val partitionSize: Int = DEFAULTS.get("kafka.partition.size").toString().toInt(),
+                              val kafkaBatchSize: Int = DEFAULTS.get("kafka.batch.size").toString().toInt(),
+                              val kafkaBufferSize: Int = DEFAULTS.get("kafka.buffer.memory").toString().toInt(),
+                              val topic: String = DEFAULTS.get("kafka.topic").toString(),
+                              val replication: Int = DEFAULTS.get("kafka.replication").toString().toInt(),
+                              val groupId : String = DEFAULTS.get("kafka.group.id").toString()) {
+
+    constructor(config: Map<String, Any>) : this(
+            config.getOrDefault("kafka.bootstrap.servers", DEFAULTS.get("kafka.bootstrap.servers")).toString(),
+            config.getOrDefault("kafka.acks", DEFAULTS.get("kafka.acks")).toString(),
+            config.getOrDefault("kafka.retries", DEFAULTS.get("kafka.retries")).toString().toInt(),
+            config.getOrDefault("kafka.partition.size", DEFAULTS.get("kafka.partition.size")).toString().toInt(),
+            config.getOrDefault("kafka.batch.size", DEFAULTS.get("kafka.batch.size")).toString().toInt(),
+            config.getOrDefault("kafka.buffer.memory", DEFAULTS.get("kafka.buffer.memory")).toString().toInt(),
+            config.getOrDefault("kafka.topic", DEFAULTS.get("kafka.topic")).toString(),
+            config.getOrDefault("kafka.replication", DEFAULTS.get("kafka.replication")).toString().toInt(),
+            config.getOrDefault("kafka.group.id", DEFAULTS.get("kafka.group.id")).toString())
+
+    companion object {
+        val DEFAULTS = mapOf(
+                "kafka.bootstrap.servers" to "localhost:9092",
+                "kafka.acks" to "1",
+                "kafka.retries" to 2,
+                "kafka.batch.size" to 16384,
+                "kafka.linger.ms" to 1,
+                "kafka.partition.size" to 1,
+                "kafka.buffer.memory" to 33554432,
+                "kafka.replication" to 1,
+                "kafka.group.id" to "neo4j",
+                "kafka.topic" to "neo4j")
+    }
+
     fun asProperties(): Properties {
         val props = Properties()
         props.put("bootstrap.servers", kafkaHosts)
--- src/main/kotlin/kafka/KafkaExtensionFactory.kt
+++ src/main/kotlin/kafka/KafkaExtensionFactory.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.neo4j.graphdb.event.TransactionEventHandler
 import org.neo4j.helpers.Service
+import org.neo4j.kernel.configuration.Config
 import org.neo4j.kernel.extension.KernelExtensionFactory
 import org.neo4j.kernel.impl.logging.LogService
 import org.neo4j.kernel.impl.spi.KernelContext
@@ -25,11 +26,12 @@ class KafkaExtensionFactory : KernelExtensionFactory<KafkaExtensionFactory.Depen
                 val userLog = log.getUserLog(KafkaModule::class.java)
                 userLog.info("Starting up")
                 try {
-                    val config = KafkaConfiguration()
+                    val neo4jConfig = db.dependencyResolver.resolveDependency(Config::class.java)
+                    val config = KafkaConfiguration(neo4jConfig.raw)
+                    val props = config.asProperties()
+                    AdminClient.create(props).use { it.createTopics(listOf(NewTopic(config.topic, config.partitionSize, config.replication.toShort()))) }
 
-                    AdminClient.create(config.asProperties()).use { it.createTopics(listOf(NewTopic(config.topic, config.partitionSize, config.replication.toShort()))) }
-
-                    val producer = KafkaProducer<Long, ByteArray>(config.asProperties())
+                    val producer = KafkaProducer<Long, ByteArray>(props)
                     userLog.info("Kafka initialization successful")
 
                     txHandler = db.registerTransactionEventHandler(KafkaModule(userLog, producer, config))
