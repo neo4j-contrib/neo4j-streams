372f3896fd5cc54f0330ded02ef461aec8b69c87
Andrea Santurbano <santand@gmail.com>
2018-12-15 01:41:10 +0100

    Rebase v 3.5 to the last version of master branch (#106)

--- consumer/src/main/kotlin/streams/StreamsEventSinkExtensionFactory.kt
+++ consumer/src/main/kotlin/streams/StreamsEventSinkExtensionFactory.kt
@@ -1,18 +1,19 @@
 package streams
 
-import org.neo4j.kernel.AvailabilityGuard
+import org.neo4j.kernel.availability.AvailabilityGuard
+import org.neo4j.kernel.availability.AvailabilityListener
 import org.neo4j.kernel.configuration.Config
+import org.neo4j.kernel.extension.ExtensionType
 import org.neo4j.kernel.extension.KernelExtensionFactory
-import org.neo4j.kernel.impl.logging.LogService
 import org.neo4j.kernel.impl.spi.KernelContext
 import org.neo4j.kernel.internal.GraphDatabaseAPI
 import org.neo4j.kernel.lifecycle.Lifecycle
 import org.neo4j.kernel.lifecycle.LifecycleAdapter
+import org.neo4j.logging.internal.LogService
 import streams.procedures.StreamsSinkProcedures
-import streams.utils.Neo4jUtils
 import streams.utils.StreamsUtils
 
-class StreamsEventSinkExtensionFactory : KernelExtensionFactory<StreamsEventSinkExtensionFactory.Dependencies>("Streams.Consumer") {
+class StreamsEventSinkExtensionFactory : KernelExtensionFactory<StreamsEventSinkExtensionFactory.Dependencies>(ExtensionType.DATABASE,"Streams.Consumer") {
 
     override fun newInstance(context: KernelContext, dependencies: Dependencies): Lifecycle {
         return StreamsEventLifecycle(dependencies)
@@ -27,43 +28,29 @@ class StreamsEventSinkExtensionFactory : KernelExtensionFactory<StreamsEventSink
 
     class StreamsEventLifecycle(private val dependencies: StreamsEventSinkExtensionFactory.Dependencies): LifecycleAdapter() {
         private val db = dependencies.graphdatabaseAPI()
-        private val logService = dependencies.log()
+        private val log = dependencies.log()
         private val configuration = dependencies.config()
-        private var streamsLog = logService.getUserLog(StreamsEventLifecycle::class.java)
+        private var streamsLog = log.getUserLog(StreamsEventLifecycle::class.java)
 
         private lateinit var eventSink: StreamsEventSink
 
         override fun start() {
             try {
-                dependencies.availabilityGuard().addListener(object: AvailabilityGuard.AvailabilityListener {
+                dependencies.availabilityGuard().addListener(object: AvailabilityListener {
                     override fun unavailable() {}
 
                     override fun available() {
-                        streamsLog.info("Initialising the Streams Sink module")
                         val streamsSinkConfiguration = StreamsSinkConfiguration.from(configuration)
                         val streamsTopicService = StreamsTopicService(db, streamsSinkConfiguration.topics)
-                        val streamsQueryExecution = StreamsEventSinkQueryExecution(streamsTopicService, db, logService.getUserLog(StreamsEventSinkQueryExecution::class.java))
-
-                        // Create and start the Sink
-                        eventSink = StreamsEventSinkFactory
-                                .getStreamsEventSink(configuration,
-                                        streamsQueryExecution,
-                                        streamsTopicService,
-                                        logService.getUserLog(StreamsEventSinkFactory::class.java))
-                        eventSink.start()
-                        if (Neo4jUtils.isWriteableInstance(db)) {
-                            if (streamsLog.isDebugEnabled) {
-                                streamsLog.debug("Subscribed topics with queries: $${streamsTopicService.getAll()}")
-                            } else {
-                                streamsLog.info("Subscribed topics: ${streamsTopicService.getTopics()}")
-                            }
-                        }
-
-                        // Register required services for the Procedures
+                        val streamsQueryExecution = StreamsEventSinkQueryExecution(streamsTopicService, db, log.getUserLog(StreamsEventSinkQueryExecution::class.java))
                         StreamsSinkProcedures.registerStreamsSinkConfiguration(streamsSinkConfiguration)
+                        eventSink = StreamsEventSinkFactory.getStreamsEventSink(configuration,
+                                streamsQueryExecution,
+                                streamsTopicService,
+                                log.getUserLog(StreamsEventSinkFactory::class.java))
+                        eventSink.start()
                         StreamsSinkProcedures.registerStreamsEventConsumerFactory(eventSink.getEventConsumerFactory())
                         StreamsSinkProcedures.registerStreamsEventSinkConfigMapper(eventSink.getEventSinkConfigMapper())
-                        streamsLog.info("Streams Sink module initialised")
                     }
 
                 })
