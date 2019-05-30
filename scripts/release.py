b018dea753174edd5050167c883b482c4fdc0d91
Mark Needham <m.h.needham@gmail.com>
2019-05-30 11:24:05 +0100

    update script

--- scripts/release.py
+++ scripts/release.py
@@ -7,7 +7,7 @@ import sys
 def upload_asset(asset_name, release_id, headers):
     with open(asset_name, "rb") as file_name_handle:
         upload_url = "https://uploads.github.com/repos/neo4j-contrib/neo4j-streams/releases/{release_id}/assets?name={asset_name}".format(
-            release_id=release_id, file_name=asset_name.split("/")[-1]
+            release_id=release_id, asset_name=asset_name.split("/")[-1]
         )
     print(upload_url)
     headers["Content-Type"] = "application/java-archive"
