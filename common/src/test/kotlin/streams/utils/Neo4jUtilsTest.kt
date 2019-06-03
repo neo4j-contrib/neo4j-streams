1f8bda6d008d084dc42033b80452b838a033b92f
Andrea Santurbano <santand@gmail.com>
2019-06-03 18:51:18 +0200

    fixes #185: Consumer not working in Cluster environment

--- common/src/test/kotlin/streams/utils/Neo4jUtilsTest.kt
+++ common/src/test/kotlin/streams/utils/Neo4jUtilsTest.kt
@@ -4,6 +4,7 @@ import org.junit.*
 import org.neo4j.kernel.impl.logging.LogService
 import org.neo4j.kernel.internal.GraphDatabaseAPI
 import org.neo4j.test.TestGraphDatabaseFactory
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class Neo4jUtilsTest {
@@ -29,6 +30,12 @@ class Neo4jUtilsTest {
     fun shouldCheckIfIsWriteableInstance() {
         val isWriteableInstance = Neo4jUtils.isWriteableInstance(db)
         assertTrue { isWriteableInstance }
+    }
+
+    @Test
+    fun shouldCheckIfIsEnterpriseEdition() {
+        val isEnterprise = Neo4jUtils.isEnterpriseEdition(db)
+        assertFalse { isEnterprise }
     }
 
 }
