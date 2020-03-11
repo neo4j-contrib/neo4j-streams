4be8668defbba3a3754650084bca55716a5fe7e1
Moxious <moxious@oldhat.org>
2020-03-11 14:52:45 -0400

    sudo docker for linux hosts

--- performance/setup.sh
+++ performance/setup.sh
@@ -6,12 +6,12 @@
 echo "
 CREATE INDEX ON :Performance(group);
 RETURN true as Producer_Is_Ready;
-" | docker exec --interactive neo4j-producer bin/cypher-shell \
+" | sudo docker exec --interactive neo4j-producer bin/cypher-shell \
     -a localhost -u neo4j -p producer
 
 # CONSUMER
 echo "
 CREATE INDEX ON :Performance(group);
 RETURN true as Consumer_Is_Ready;
-" | docker exec --interactive neo4j-consumer bin/cypher-shell \
+" | sudo docker exec --interactive neo4j-consumer bin/cypher-shell \
     -a localhost -u neo4j -p consumer
