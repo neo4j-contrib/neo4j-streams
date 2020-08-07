fd292b431cb9a1796be45aedd14c0c2bbb49a05c
Moxious <moxious@oldhat.org>
2020-08-07 11:45:27 -0400

    changes necessary for local docs build

--- doc/server.js
+++ doc/server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('./build/site'))
 
-app.get('/', (req, res) => res.redirect('/neo4j-streams-docs/4.0.2'))
+app.get('/', (req, res) => res.redirect('/kafka/4'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
