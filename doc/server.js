1653fd98f5838f129371468dc874618ed8f3f494
Moxious <moxious@oldhat.org>
2020-09-01 11:57:57 -0400

    fix URL

--- doc/server.js
+++ doc/server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('./build/site'))
 
-app.get('/', (req, res) => res.redirect('/kafka/4'))
+app.get('/', (req, res) => res.redirect('/kafka/4.0'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
